a34cf2aea70bfd902da3d849688275779294e7bf
yarchenkoserhii-jpg <yarchenkoserhii@gmail.com>
2025-09-24 15:51:47 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+function UAStream() {
+    return {
+        title: 'UAStream 🇺🇦',
+        items: [
+            {
+                title: 'Довбуш (2023)',
+                url: 'https://example.com/dovbush.mp4',
+                poster: 'https://i.imgur.com/zYwQwqL.jpeg'
+            },
+            {
+                title: 'Кіборги',
+                url: 'https://example.com/kiborgy.mp4',
+                poster: 'https://i.imgur.com/VX4uP1k.jpeg'
+            },
+            {
+                title: 'Наші Котики',
+                url: 'https://example.com/kotyky.mp4',
+                poster: 'https://i.imgur.com/0Q8mR0S.jpeg'
+            },
+            {
+                title: 'І будуть люди (серіал)',
+                url: 'https://example.com/ludy.mp4',
+                poster: 'https://i.imgur.com/6b2k3uU.jpeg'
+            }
+        ]
+    };
+}
+
+UAStream();
